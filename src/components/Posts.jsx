a2c81fe588517a5c8a47e15405ae942d30ae48b9
thedrops <pedrops02@gmail.com>
2019-09-05 20:34:51 -0300

    salvar posts/ add biblioteca axios

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -2,13 +2,23 @@ import React , {Component} from 'react';
 import '../App.css';
 import objeto from '../posts.json';
 import Post from './Post';
+import axios from 'axios';
 
 class Posts extends React.Component{
   constructor(props){
     super(props)
     this.state = {
-      posts:objeto.posts
+      posts:[],
+      titulo : "",
+      url : "",
     }
+
+    axios.get('http://www.mocky.io/v2/5d7193e0330000f7cf7799a1')
+    .then((result) => {
+      this.setState({
+        posts:result.data.posts
+      })
+    });
 
     this._excluirPost = this._excluirPost.bind(this);
   }
@@ -19,8 +29,28 @@ class Posts extends React.Component{
     })
 
     this.setState({posts: newPosts})
-    
   }
+
+  _salvarPost(){
+    var newId = this.state.posts[this.state.posts.length-1].id+1
+    var newPost = {
+      id:newId,
+      titulo:this.state.titulo,
+      url:this.state.url,
+    }
+
+    var posts = this.state.posts
+
+    posts.push(newPost)
+
+    this.setState({
+      posts:posts,
+      titulo : "",
+      url : ""
+    })
+
+  }
+
 
   render(){
   return(
@@ -36,6 +66,19 @@ class Posts extends React.Component{
         />
       )
     }
+    <div>
+      <span>Título: </span>
+      <input class="form-control" value={this.state.titulo} onChange={(e) => {
+        this.setState({titulo : e.target.value})
+        } }/>
+    </div>
+    <div>
+      <span>URL: </span>
+      <input class="form-control" value={this.state.url} onChange={(e) => {
+        this.setState({url : e.target.value})
+        } }/>
+    </div>
+    <button class="btn btn-primary" onClick={() => { this._salvarPost()}}>Salvar</button>
     </div>
   )
   }
